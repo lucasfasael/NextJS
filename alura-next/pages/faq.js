1b80ab26e0781e817e23691fb1d99911fd829979
Lucas Fasael <lucas_fasael@hotmail.com>
2025-01-21 12:25:45 -0300

    faq, 404

---
+++ alura-next/pages/faq.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+export default function FaqPage () {
+    return (
+        <div>
+            <h1>
+                FAQ PAGE
+            </h1>
+            <Link href="/">
+                Ir para a home
+            </Link>
+            
+            
+        </div>
+    )
+}
