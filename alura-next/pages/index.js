d1255228764e04197848334508856381151c20e6
Lucas <lucas_fasael@hotmail.com>
2025-01-21 21:51:51 -0300

    global style

--- alura-next/pages/index.js
+++ alura-next/pages/index.js
@@ -1,9 +1,31 @@
 import Link from "next/link";
+import React from 'react';
+
+function Title({children, as}){
+
+    const Tag = as;
+
+    return (
+        <React.Fragment>
+            <Tag>
+                {children}
+            </Tag>
+            <style jsx>{`
+                    ${Tag}{
+                    color: red;
+                    font-family: sans-serif;
+                    }
+                `}
+            </style>
+        </React.Fragment>
+    );
+}
 
 export default function HomePage () {
     return (
-        <div> 
-        <h1>Alura Cases - HomePage </h1>
+        <div>
+        
+        <Title as="h1">Alura Cases - HomePage </Title>
         <Link href="/faq">
         Ir para o FAQ
         </Link>
